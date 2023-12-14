532484dc967811e0ca1da131be89864b402c3805
Royce Calvin <royce20002006@yahoo.com>
2023-12-13 19:35:20 -0600

    05

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -9,7 +9,16 @@ Examples:
 arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
+const arrowMirrorArray = array => {
+  let newArr = [];
 
+  array.forEach((el) => newArr.push(el)) 
+  for (let i = array.length -  1; i >= 0; i--) {
+    let el = array[i];
+    newArr.push(el);
+  }
+  return newArr;
+}
 // Your code here 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
